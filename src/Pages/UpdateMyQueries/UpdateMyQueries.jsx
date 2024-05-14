8ad48753a7fa77653e20f03570dc7e8bca5935c6
Bithe <farzana.hossain5@gmail.com>
2024-05-14 10:09:28 +0600

    updated my query done

--- src/Pages/UpdateMyQueries/UpdateMyQueries.jsx
+++ src/Pages/UpdateMyQueries/UpdateMyQueries.jsx
@@ -1,19 +1,72 @@
 import { FaProductHunt, FaTrademark, FaImage, FaQuestionCircle, FaExclamationCircle } from 'react-icons/fa';
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../components/AuthProvider/AuthProvider';
 import { useContext } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 
 
 const UpdateMyQueries = () => {
   const { user } = useContext(AuthContext);
 
-  const navigate= useNavigate();
+  // const navigate = useNavigate();
 
   const query = useLoaderData();
   console.log(query);
 
-  const {_id, productName } = query;
+  const {_id, productName, productBrand, productImage, queryTitle, boycottingReason } = query;
+
+
+  const handleUpdateMyQuery = async (e) => {
+    e.preventDefault();
+    console.log(user.email, user.displayName);
+
+    // get the email a& name from firebase
+    const email = user.email;
+    const userName = user.displayName;
+    const userPhoto = user?.photoURL;
+    const currentDateTime = new Date().toISOString();
+
+    // get the values from input filed
+    const productName = e.target.productName.value;
+    const productBrand = e.target.productBrand.value;
+    const productImage = e.target.productImage.value;
+    const queryTitle = e.target.queryTitle.value;
+    const boycottingReason = e.target.boycottingReason.value;
+
+    const updatedQueriesInfo = {
+      productName,
+      productBrand,
+      productImage,
+      queryTitle,
+      boycottingReason,
+
+      addQueriesUserInfo: {
+        email,
+        userName,
+        userPhoto,
+        dateTime: currentDateTime,
+        recommendationCount: 0,
+      },
+    };
+
+    console.log(updatedQueriesInfo);
+
+    //MAKE THE API
+    try {
+      // Send the form data to the backend server
+      const { data } = await axios.put(
+        `http://localhost:5000/query/${_id}`,
+        updatedQueriesInfo
+      );
+      console.log(data);
+      toast("Query Updated successfully!");
+    } catch (error) {
+      console.error("Error Updating query:", error);
+      toast.error("Error updating query. Please try again later.");
+    }
+  };
     return (
       <section className='container mx-auto lg:px-20 lg:py-8 '>
           <div className="mt-10 p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg my-8">
@@ -21,7 +74,7 @@ const UpdateMyQueries = () => {
           Update Your Query
         </h2>
         <p></p>
-        <form id="queryForm">
+        <form onSubmit={handleUpdateMyQuery} id="queryForm">
           <div className="grid grid-cols-2 gap-4 mb-4">
             <div className="flex items-center">
               <FaProductHunt className="text-gray-700 dark:text-gray-400 mr-2" />
@@ -39,7 +92,7 @@ const UpdateMyQueries = () => {
             <div className="flex items-center">
               <FaTrademark className="text-gray-700 dark:text-gray-400 mr-2" />
               <input
-              defaultValue={query.productBrand}
+              defaultValue={productBrand}
                 type="text"
                 id="productBrand"
                 name="productBrand"
@@ -51,7 +104,7 @@ const UpdateMyQueries = () => {
             <div className="flex items-center">
               <FaImage className="text-gray-700 dark:text-gray-400 mr-2" />
               <input
-              defaultValue={query.productImage}
+              defaultValue={productImage}
                 type="url"
                 id="productImage"
                 name="productImage"
@@ -63,7 +116,7 @@ const UpdateMyQueries = () => {
             <div className="flex items-center">
               <FaQuestionCircle className="text-gray-700 dark:text-gray-400 mr-2" />
               <input
-              defaultValue={query.queryTitle}
+              defaultValue={queryTitle}
                 type="text"
                 id="queryTitle"
                 name="queryTitle"
@@ -75,7 +128,7 @@ const UpdateMyQueries = () => {
             <div className="flex items-center">
               <FaExclamationCircle className="text-gray-700 dark:text-gray-400 mr-2" />
               <textarea
-              defaultValue={query.boycottingReason}
+              defaultValue={boycottingReason}
                 id="boycottingReason"
                 name="boycottingReason"
                 placeholder="Boycotting Reason Details"
